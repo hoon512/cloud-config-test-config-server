6f20aea11feb05923f458cb652b10a8799766895
hoon512 <go-lang@naver.com>
2024-10-23 00:34:37 +0900

    init

---
+++ src/main/kotlin/com/hoon512/playground/cloudconfigtestconfigserver/CloudConfigTestConfigServerApplication.kt
@@ -0,0 +1,11 @@
+package com.hoon512.playground.cloudconfigtestconfigserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CloudConfigTestConfigServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<CloudConfigTestConfigServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/hoon512/playground/cloudconfigtestconfigserver/CloudConfigTestConfigServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hoon512.playground.cloudconfigtestconfigserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CloudConfigTestConfigServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
