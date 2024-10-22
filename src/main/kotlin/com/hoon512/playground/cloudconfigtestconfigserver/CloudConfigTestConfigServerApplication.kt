1a7f8fb90e71a56113f4b1e7690dac18a59b55f8
hoon512 <go-lang@naver.com>
2024-10-23 00:40:31 +0900

    config server 기본 세팅

--- src/main/kotlin/com/hoon512/playground/cloudconfigtestconfigserver/CloudConfigTestConfigServerApplication.kt
+++ src/main/kotlin/com/hoon512/playground/cloudconfigtestconfigserver/CloudConfigTestConfigServerApplication.kt
@@ -2,8 +2,10 @@ package com.hoon512.playground.cloudconfigtestconfigserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
 
 @SpringBootApplication
+@EnableConfigServer
 class CloudConfigTestConfigServerApplication
 
 fun main(args: Array<String>) {
